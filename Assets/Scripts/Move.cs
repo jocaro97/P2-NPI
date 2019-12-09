6aafedb56dd048b99029167038612be437795f38
jocaro97 <johanna_capote_robayna@hotmail.com>
2019-12-09 18:06:17 +0100

    Empezando

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+    public bool turnLeft;
+    public bool turnRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnLeft = false;
+        turnRight = false;
+    }
+
+    void FixedUpdate () {
+
+        var c = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(c);
+        Debug.Log(SceneManager.sceneCountInBuildSettings);
+        // LoadSceneAsync
+        if (turnLeft) {
+            var next = (c-1+SceneManager.sceneCountInBuildSettings)%SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(next);
+            //transform.Rotate(new Vector3(10, 0, 0) * Time.deltaTime);
+        }
+        if (turnRight) {
+            var next = (c+1+SceneManager.sceneCountInBuildSettings)%SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(next);
+            //transform.Rotate(new Vector3(-10, 0, 0) * Time.deltaTime);
+        }
+    }
+}
