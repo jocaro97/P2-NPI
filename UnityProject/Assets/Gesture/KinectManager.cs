5ab4bdaea0633cb4874ef58ab5ad6744da4c33b6
jocaro97 <johanna_capote_robayna@hotmail.com>
2019-12-12 17:46:21 +0100

    cambiando el nombre pal guishe

--- UnityProject/Assets/Gesture/KinectManager.cs
+++ UnityProject/Assets/Gesture/KinectManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kinnectManager : MonoBehaviour
+public class KinectManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
