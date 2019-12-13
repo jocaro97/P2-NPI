72aa8679fab0232971ac01241942b8b2f124e946
Guille <guillegalor@gmail.com>
2019-12-13 17:55:54 +0100

    Test added

---
+++ Test/Assets/KinectView/Segments/RotateRightGestureSegments.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+namespace KinectSimpleGesture
+{
+   
+    public class RotateRightSegment1 : IGestureSegment
+    {
+        public GesturePartResult Update(Body skeleton)
+        {
+            if (
+                skeleton.Joints[JointType.HandRight].Position.X > skeleton.Joints[JointType.ElbowRight].Position.X
+                &&
+                skeleton.Joints[JointType.HandLeft].Position.X > skeleton.Joints[JointType.ElbowLeft].Position.X
+                )
+            {
+                return GesturePartResult.Succeeded;
+            }
+            return GesturePartResult.Failed;
+        }
+    }
+
+    public class RotateRightSegment2 : IGestureSegment
+    {
+        public GesturePartResult Update(Body skeleton)
+        {
+            if (
+                skeleton.Joints[JointType.HandRight].Position.X < skeleton.Joints[JointType.ElbowRight].Position.X
+                &&
+                skeleton.Joints[JointType.HandLeft].Position.X < skeleton.Joints[JointType.ElbowLeft].Position.X
+                )
+            {
+                return GesturePartResult.Succeeded;
+            }
+            return GesturePartResult.Failed;
+        }
+    }
+}
