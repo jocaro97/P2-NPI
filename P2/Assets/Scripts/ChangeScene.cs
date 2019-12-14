bf9e9434ecb0aa9f28f8236e55105aae5dd658b4
jocaro97 <johanna_capote_robayna@hotmail.com>
2019-12-14 12:25:27 +0100

    Añadidios changescene y zoom

---
+++ P2/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public bool next;
+    public bool prev;
+    void Start()
+    {
+        next = false;
+        prev = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var c = SceneManager.GetActiveScene().buildIndex;
+        // LoadSceneAsync
+        if (prev)
+        {
+            var next = (c - 1 + SceneManager.sceneCountInBuildSettings) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(next);
+        }
+        if (next)
+        {
+            var next = (c + 1 + SceneManager.sceneCountInBuildSettings) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(next);
+        }
+    }
+}
---
+++ P2/Assets/Scripts/Zoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Zoom : MonoBehaviour
+{
+    public bool zoomIn;
+    public bool zoomOut;
+    public float zoomMultiplier = 1.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zoomIn = false;
+        zoomOut = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var zoomAmount = Time.deltaTime * zoomMultiplier;
+
+        if (zoomIn)
+        {
+            transform.Translate(zoomAmount, zoomAmount, zoomAmount, Space.World);
+        }
+        if (zoomOut)
+        {
+            transform.Translate(-zoomAmount, -zoomAmount, -zoomAmount, Space.World);
+        }
+    }
+
+}
