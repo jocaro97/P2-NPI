228abae80f7d297c1cfdc6070e906021fd1f4a45
Guille <guillegalor@gmail.com>
2019-12-16 22:56:23 +0100

    Changes

--- P2/Assets/Gesture Scripts/Segments/NextGestureSegments.cs
+++ P2/Assets/Gesture Scripts/Segments/NextGestureSegments.cs
@@ -30,11 +30,9 @@ namespace KinectSimpleGesture
         public GesturePartResult Update(Body skeleton)
         {
             if (
-                skeleton.HandRightState == HandState.Open
+                skeleton.Joints[JointType.HandTipRight].Position.X < skeleton.Joints[JointType.ElbowRight].Position.X 
                 &&
-                skeleton.Joints[JointType.HandTipRight].Position.X < skeleton.Joints[JointType.ElbowRight].Position.X - 0.01
-                &&
-                skeleton.Joints[JointType.HandTipRight].Position.Y > skeleton.Joints[JointType.SpineShoulder].Position.Y
+                skeleton.Joints[JointType.HandTipRight].Position.Y > skeleton.Joints[JointType.ElbowRight].Position.Y
                 &&
                 skeleton.Joints[JointType.HandTipLeft].Position.Y < skeleton.Joints[JointType.SpineMid].Position.Y
                 )
